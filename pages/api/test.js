9b3a3906cf877727b7a3cb0ae2f3a2629712a5bf
EmenikeIsGreat <emenikeani3@gmail.com>
2022-06-24 17:22:59 -0400

    Add files via upload

---
+++ pages/api/test.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const user = require('./user')
+mongoose.connect(user).then((result) =>{
+    console.log("connected")
+}).catch((error) =>{
+    console.log(error)
+})
+
+
+
+export default function handler(req, res) {
+    try{
+        let message = User.create({
+             UserID:"EmenikeID2",
+             firstName:"Emenike",
+             lastName:"Anigbogu",
+             userName:"DarkChocolate",
+             email:"emenikeani3@gmail.com",
+             phoneNumber:"6172869610"
+             })
+         console.log(message)
+     }
+ 
+     catch(error){
+         console.log(error);
+     }
+    res.status(200).json(message)
+  }
