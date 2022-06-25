4fea6481e2af25d801bd817bfe601a2c1206bd9a
EmenikeIsGreat <emenikeani3@gmail.com>
2022-06-25 11:17:13 -0400

    Update BlockChainEventHandler.js

--- pages/api/BlockChainEventHandler.js
+++ pages/api/BlockChainEventHandler.js
@@ -1,3 +1,19 @@
-export default function handler(req, res) {
-  res.status(200).json(req.body)
+export default async function handler(req, res){
+    try{
+       let transaction = await transfers.create({
+            UserID: "Emenike",
+            Transaction:{
+                TransactionID:"123",
+                senderID: "Emenike",
+                recipientID: "Arinze",
+                AssetID:"testCoin",
+                AssetAmount:100
+            }
+        })
+        res.status(200).json(req.body)
+    }
+
+    catch(error){
+        console.log(error);
+    }
 }
